92970fceb5c7ff841106cc1ff0f32d5e29ff5d78
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-04-30 18:30:04 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,30 +1,30 @@
-class PigLatinizer
+# class PigLatinizer
   
-  def piglatinize(str)
-    str.split(" ").length == 1 ? piglatinize_word(str) : piglatinize_sent(str)
-  end
+#   def piglatinize(str)
+#     str.split(" ").length == 1 ? piglatinize_word(str) : piglatinize_sent(str)
+#   end
   
-  private
+#   private
   
-  def consonant?(char)
-    !char.match[/[aeiouAEIOU]/]
-  end
+#   def consonant?(char)
+#     !char.match[/[aeiouAEIOU]/]
+#   end
     
-  def piglatinize_word(word)
-    if !consonant?(word[0])
-      word = word + "w"
-    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
-      word = word.slice(3..-1) + word.slice(0,3)
-    elsif consonant?(word[0]) && consonant?(word[1])
-      word = word.slice(2..-1) + word.slice(0,2)
-    else
-      word = word.slice(1..-1) + word.slice(0)
-    end
-    word << "ay"
-  end
+#   def piglatinize_word(word)
+#     if !consonant?(word[0])
+#       word = word + "w"
+#     elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+#       word = word.slice(3..-1) + word.slice(0,3)
+#     elsif consonant?(word[0]) && consonant?(word[1])
+#       word = word.slice(2..-1) + word.slice(0,2)
+#     else
+#       word = word.slice(1..-1) + word.slice(0)
+#     end
+#     word << "ay"
+#   end
   
-  def piglatinize_sentence(sent)
-    sent.split.collect { |word| piglatinize_word(word) }.join(" ")
-  end
+#   def piglatinize_sentence(sent)
+#     sent.split.collect { |word| piglatinize_word(word) }.join(" ")
+#   end
   
-end
+# end
