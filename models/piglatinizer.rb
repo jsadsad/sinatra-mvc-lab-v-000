82a07fd564cbe6c26402bc71f151173e255eabc7
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-04-30 18:28:17 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -16,7 +16,7 @@ class PigLatinizer
     elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
       word = word.slice(3..-1) + word.slice(0,3)
     elsif consonant?(word[0]) && consonant?(word[1])
-      word  = word.slice(2..-1) + word.slice(0,2)
+      word = word.slice(2..-1) + word.slice(0,2)
     else
       word = word.slice(1..-1) + word.slice(0)
     end
