42750d3599e7547b55ea7982364ebc5497bbf1ae
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-04-30 18:22:30 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,6 +1,6 @@
 class PigLatinizer
   def piglatinize(str)
-    str.split(" ").length == 1 ? piglatinze_word(str) : piglatinize_sent(str)
+    str.split(" ").length == 1 ? piglatinize_word(str) : piglatinize_sent(str)
   end
   
   private
